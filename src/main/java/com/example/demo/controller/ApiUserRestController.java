e32cf4931eedde3a23de4c6ba7dcae34b7892e11
sunghwan-joo <sunghwan.jooo@gmail.com>
2024-10-02 17:39:09 +0900

    commit

--- src/main/java/com/example/demo/controller/ApiUserRestController.java
+++ src/main/java/com/example/demo/controller/ApiUserRestController.java
@@ -1,25 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ApiUserDto;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiUserRestController {
-	
-	public ApiUserRestController() {
+    
+    public ApiUserRestController() {
     }
 
     @GetMapping("/api/v1/user")
     public ApiUserDto getApiUserId() {
-    	ApiUserDto user = new ApiUserDto();
+        ApiUserDto user = new ApiUserDto();
         return user;
     }
 }
